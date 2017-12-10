8c4fbccec1858e523e16ac8dbaa2c00cf1507b23
Jackie <Jackie880823@gmail.com>
2017-12-10 21:54:54 +0800

    Optimize: 优化选择的数据

--- app/src/main/java/com/jackie/gallery/MainActivity.java
+++ app/src/main/java/com/jackie/gallery/MainActivity.java
@@ -1,5 +1,8 @@
 package com.jackie.gallery;
 
+import android.content.ClipData;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -8,7 +11,14 @@ import android.view.View;
 
 import com.madxstudio.libs.tools.gallery.GalleryFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_GALLERY_CODE = 1000;
+
+    private List<Uri> uris = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +31,28 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                GalleryFactory.init().limitCount(9).limitFileSize(20 * 1024 * 1024).needVideo(true).fetch(MainActivity.this,
-                        1000);
+                GalleryFactory.init().limitCount(9).limitFileSize(20 * 1024 * 1024).needVideo(true).postHave()
+                        .setSelected(uris).fetch(MainActivity.this, REQUEST_GALLERY_CODE);
+                uris.clear();
             }
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK) {
+
+            return;
+        }
+        switch (requestCode) {
+            case REQUEST_GALLERY_CODE:
+                ClipData clipData = data.getClipData();
+                int itemCount = clipData.getItemCount();
+                for (int i = 0; i < itemCount; i++) {
+                    uris.add(clipData.getItemAt(i).getUri());
+                }
+                break;
+        }
+    }
 }
